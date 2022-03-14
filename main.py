af6330749a9d523ebce5169f0eb91818cce67db7
swapnilarya <swapnilarya68@gmail.com>
2022-03-14 10:03:41 -0700

    Initial commit

--- main.py
+++ main.py
@@ -17,14 +17,23 @@ class CustomDaemon(Daemon):
 
 if __name__ == '__main__':
 
-    logging.basicConfig(filename='app.log',
-                        filemode='a',
-                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s  - %(message)s \n',
-                        datefmt='%H:%M:%S',
-                        level=logging.DEBUG)
+    APP_LOGGER = 'custom_logger'
+
+    # Initialize Logger
+    logger = logging.getLogger(APP_LOGGER)
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('app.log')
+    fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s -> %(message)s\n')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
 
     daemon = CustomDaemon()
-    usageMessage = f"Usage: {sys.argv[0]} (start|stop|restart|status|reload|version)"
+    usageMessage = f"Usage: {sys.argv[0]} (start|stop|restart|status)"
     if len(sys.argv) == 2:
         choice = sys.argv[1]
         if choice == "start":
@@ -35,12 +44,7 @@ if __name__ == '__main__':
             daemon.restart()
         elif choice == "status":
             daemon.status()
-        elif choice == "reload":
-            daemon.reload()
-        elif choice == "version":
-            daemon.version()
         else:
-            print("Unknown command.")
             print(usageMessage)
             sys.exit(1)
         sys.exit(0)
